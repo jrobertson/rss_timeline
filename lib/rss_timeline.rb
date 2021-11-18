bc73e64c0dd08f6578fece9157d7fdf944c8ddd1
James Robertson <james@jamesrobertson.me.uk>
2021-11-18 20:14:36 +0000

    bug fix: URI.open now explicitly stated, as required in Ruby 3.0.2.

--- lib/rss_timeline.rb
+++ lib/rss_timeline.rb
@@ -9,12 +9,12 @@ require 'rss_creator'
 
 
 class RSStimeline
-  
+
   attr_accessor :rssfile
 
-  def initialize(feeds=[], rssfile: 'timeline.rss', xslt: nil, 
+  def initialize(feeds=[], rssfile: 'timeline.rss', xslt: nil,
                  filepath: '.', debug: false, target_filepath: nil)
-    
+
     @source_feeds, @debug, @rssfile, @newupdate = feeds, debug, rssfile, false
     @target_filepath = target_filepath
 
@@ -27,15 +27,15 @@ class RSStimeline
     FileUtils.mkdir_p @cache_filepath
 
     if File.exists? rssfile then
-      @timeline =RSScreator.new rssfile
-    else      
+      @timeline = RSScreator.new rssfile
+    else
 
-      @timeline =RSScreator.new
+      @timeline = RSScreator.new
       self.title = 'My RSStimeline feed'
-      self.description = 'Generated using the RSStimeline gem'            
-      
+      self.description = 'Generated using the RSStimeline gem'
+
     end
-    
+
     @timeline.xslt = xslt if xslt
     puts '@timeline.xslt : ' + @timeline.xslt.inspect if @debug
 
@@ -44,19 +44,19 @@ class RSStimeline
   def update()
 
     # fetch the feeds from the web
-    feeds = @source_feeds.map {|feed| [feed, SimpleRSS.parse(open(feed))] }
+    feeds = @source_feeds.map {|feed| [feed, SimpleRSS.parse(URI.open(feed))] }
 
     # check for each feed from the cache.
     # if the feed is in the cache, compare the 2 to find any new items.
     # New items should be added to the main timeline RSS feed
-    
+
     updated = false
 
     feeds.each do |feed, rss|
 
       rssfile = File.join(@cache_filepath, feed[6..-1].gsub(/\W+/,'').\
                                              reverse.slice(0,40).reverse)
-      
+
       if File.exists? rssfile then
 
         rss_cache = SimpleRSS.parse File.read(rssfile)
@@ -64,105 +64,112 @@ class RSStimeline
         fresh, old = [rss.items, rss_cache.items].map do |feed|
           feed.clone.each {|x| x.delete :guid }
         end
-        
+
+
         new_items = fresh - old
-        
+
+        if @debug then
+          puts 'fresh: ' + fresh.inspect
+          puts 'old: ' + old.inspect
+          puts 'new_items: ' + new_items.inspect
+        end
+
         new_rss_items = new_items.map do |x|
           rss.items.find {|y| y[:title] == x[:title]}
         end
-        
+
         new_rss_items.reverse.each {|item| add_new item}
-        
+
         if new_rss_items.any? then
           puts 'new_rss_items: ' + new_rss_items.inspect if @debug
           updated = true
-          File.write rssfile, rss.source 
+          File.write rssfile, rss.source
         end
-        
+
       else
 
         updated = true
         add_new rss.items.first
         File.write rssfile, rss.source
-        
+
       end
     end
-        
+
     save() if updated
-    
+
   end
-  
+
   def description()
     @timeline.description
   end
-  
+
   def description=(val)
     @timeline.description = val
   end
-  
+
   def link(val)
     @timeline.link
   end
-  
+
   def link=(val)
     @timeline = val
   end
-  
+
   def title()
     @timeline.title
   end
-  
+
   def title=(val)
     @timeline.title = val
   end
-  
+
   protected
-  
+
   def on_new_item(item)
-    # you can override this method to create your 
+    # you can override this method to create your
     #                                  own notifier, callback, or webhook
   end
-  
+
   def on_update()
-    # you can override this method to create your 
+    # you can override this method to create your
     #                                  own notifier, callback, or webhook
-  end    
-  
+  end
+
   private
-  
+
   def add_new(item)
-    
+
     puts 'inside add_new: ' + item.inspect if @debug
-    
+
     @timeline.add item: new_item(item), id: nil
     @newupdate = true
     on_new_item(item)
-    
+
   end
-  
+
   def new_item(x)
-    
+
     h = {
-      title: x[:title], 
-      link: x[:link], 
-      description: x[:description], 
+      title: x[:title],
+      link: x[:link],
+      description: x[:description],
       date: x[:date] || Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")
     }
-    
+
     puts 'inside new_item: ' + h.inspect if @debug
-    
+
     h
-    
+
   end
-  
+
   def save()
-        
+
     @newupdate = false
-    
+
     @timeline.save File.join(@filepath, @rssfile)
     @timeline.save File.join(@target_filepath, @rssfile) if @target_filepath
-    on_update()    
-    
+    on_update()
+
   end
-  
+
 end
--- rss_timeline.gemspec
+++ rss_timeline.gemspec
@@ -1,14 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'rss_timeline'
-  s.version = '0.2.1'
+  s.version = '0.2.2'
   s.summary = 'An RSS aggregator which generates an RSS file'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rss_timeline.rb']
-  s.add_runtime_dependency('simple-rss', '~> 1.3', '>=1.3.1')
-  s.add_runtime_dependency('rss_creator', '~> 0.3', '>=0.3.11')
+  s.add_runtime_dependency('simple-rss', '~> 1.3', '>=1.3.3')
+  s.add_runtime_dependency('rss_creator', '~> 0.4', '>=0.4.2')
   s.signing_key = '../privatekeys/rss_timeline.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/rss_timeline'
 end
